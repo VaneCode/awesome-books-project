c27ba3f6c99a7b4a962768cb3b84b3a20fb852d8
Vanessa Bonito <53547956+VaneCode@users.noreply.github.com>
2022-07-07 10:19:57 -0500

    Udpate date format in classes-books.js file

--- app/js/classes-books.js
+++ app/js/classes-books.js
@@ -152,18 +152,7 @@ window.addEventListener('load', () => {
 
 
 // date format
-var objToday = new Date(),
-	weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
-	dayOfWeek = weekday[objToday.getDay()],
-	domEnder = function() { var a = objToday; if (/1/.test(parseInt((a + "").charAt(0)))) return "th"; a = parseInt((a + "").charAt(1)); return 1 == a ? "st" : 2 == a ? "nd" : 3 == a ? "rd" : "th" }(),
-	dayOfMonth = today + ( objToday.getDate() < 10) ? '0' + objToday.getDate() + domEnder : objToday.getDate() + domEnder,
-	months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
-	curMonth = months[objToday.getMonth()],
-	curYear = objToday.getFullYear(),
-	curHour = objToday.getHours() > 12 ? objToday.getHours() - 12 : (objToday.getHours() < 10 ? "0" + objToday.getHours() : objToday.getHours()),
-	curMinute = objToday.getMinutes() < 10 ? "0" + objToday.getMinutes() : objToday.getMinutes(),
-	curSeconds = objToday.getSeconds() < 10 ? "0" + objToday.getSeconds() : objToday.getSeconds(),
-	curMeridiem = objToday.getHours() > 12 ? "PM" : "AM";
-  var today = curHour + ":" + curMinute + "." + curSeconds + curMeridiem + " " + dayOfWeek + " " + dayOfMonth + " of " + curMonth + ", " + curYear;
-  var awesomeDate = curMonth +' '+ dayOfMonth +' '+ curYear +', '+ curHour + ":" + curMinute + ":" + curSeconds +' '+ curMeridiem ;
-  document.querySelector(".date").innerHTML = awesomeDate;
+// date format
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const date = new Date();
+document.querySelector('.date').innerHTML = `${months[date.getMonth()]} ${date.getDate()}  ${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}`;
